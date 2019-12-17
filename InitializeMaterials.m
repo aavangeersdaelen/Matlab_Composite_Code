c1b65b3b4d00758d2364935368afec9eb3577c0b
aavangeersdaelen <aavangeersdaelen@crimson.ua.edu>
2019-12-16 20:47:03 -0600

    Added Materials folder for organization

--- InitializeMaterials.m
+++ InitializeMaterials.m
@@ -1,6 +1,7 @@
 %Initialize Project
 %Tony VanGeersdaelen
 clc;clear;close;
+addpath(fullfile(pwd,'Materials'));
 load('UniCarbonEpoxy.mat');
 load('UniGlassEpoxy.mat');
 load('WeaveCarbonEpoxy.mat');
