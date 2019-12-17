3951eb8f581fd168e9b2feff7d733b6f1c29eb7c
aavangeersdaelen <aavangeersdaelen@crimson.ua.edu>
2019-12-16 20:35:01 -0600

    Add Initialize Script

---
+++ InitializeMaterials.m
@@ -0,0 +1,6 @@
+%Initialize Project
+%Tony VanGeersdaelen
+clc;clear;close;
+load('UniCarbonEpoxy.mat');
+load('UniGlassEpoxy.mat');
+load('WeaveCarbonEpoxy.mat');
--- Materials.m
+++ Materials.m
@@ -1,8 +1,8 @@
 clc;clear;close;
 
-UniCarbonEpoxy=Comp(22e6,1.5e6,1e6,.3,.0007);
-WeaveCarbonEpoxy=Comp(9e6,9e6,1e6,.05,.01);
-UniGlassEpoxy=Comp(6e6,1.5e6,.8e6,.3,.007);
+UniCarbonEpoxy=Comp('UniCarbon',22e6,1.5e6,1e6,.3,.0007);
+WeaveCarbonEpoxy=Comp('WeaveCarbon',9e6,9e6,1e6,.05,.01);
+UniGlassEpoxy=Comp('UniGlass',6e6,1.5e6,.8e6,.3,.007);
 
 
 
